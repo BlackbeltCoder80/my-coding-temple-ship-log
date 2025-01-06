805d6ade7e44335aa5b1f7b14865b36a05c9ad7f
BlackBeltCoder80 <blackbeltcoder80@gmail.com>
2025-01-06 01:47:14 -0700

    New Update

---
+++ PracticeCoding.py
@@ -0,0 +1,7 @@
+# Defining variable and defining it as true
+torch_lit = True
+if torch_lit == True:
+    # We are wiritng an if statment . If torch_lit is True, We want the print statment to run
+    print("venture forth into the cave!")
+
+    
