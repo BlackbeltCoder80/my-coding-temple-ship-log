582a312b0554ac52a953ede671c8040067166c53
BlackBeltCoder80 <blackbeltcoder80@gmail.com>
2025-01-06 19:01:24 -0700

    updating

--- PracticeCoding.py
+++ PracticeCoding.py
@@ -4,4 +4,10 @@ if torch_lit == True:
     # We are wiritng an if statment . If torch_lit is True, We want the print statment to run
     print("venture forth into the cave!")
 
-    
+    weather = "sunny"
+    print ("Tome for a picnic!")
+
+    gold_coins = 10
+    silver_coins = 50
+    if gold_coins > 5 and silver_coins > 30:
+        print("Enough to buy the magic potion!")
