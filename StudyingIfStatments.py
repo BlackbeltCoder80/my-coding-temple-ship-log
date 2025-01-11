bdcb6df1a7371435c2efac26f5adf04ad8ccc36a
BlackBeltCoder80 <blackbeltcoder80@gmail.com>
2025-01-10 23:03:54 -0700

    Updated

---
+++ StudyingIfStatments.py
@@ -0,0 +1,7 @@
+# Defining a variable and setting it to True
+torch_lit = True
+
+# We are writing an if statment. If torch_lit is True, we want the print to run
+if torch_lit == True:
+    print("Venture forth into the cave!")
+    
