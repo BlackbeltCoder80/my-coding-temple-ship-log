164f5468fb34a7986ff941b84dccaa939e98e8ae
BlackBeltCoder80 <blackbeltcoder80@gmail.com>
2025-01-13 00:53:40 -0700

    Updated

--- PracticeCoding1.py
+++ PracticeCoding1.py
@@ -1,8 +1,19 @@
+#Import Time module to track time
 import datetime
-datetime = input("What time is it?: ")
+#ask user to imput time
+datetime = input("What time is it?: ") # Output "what time is it?"
+# What is said when it is morning time 12:00am-11:00am
 morning = "Time to wake up!"
+# What is said when it is afternoon time 12:00pm-4:00pm
 afternoon = "Time to go outside"
-evening = "We can play games now" if datetime == "18:00" else "We can't play game until 18:00"
+# What is said, when it is evening time 5:00pm-6:00pm
+evening = "We can play games now" 
+
+# 
+if datetime == 
+    print()
+elif "We can't play game until 18:00":
+
 print (evening) if datetime < "06:00" >= "12:01" else "Check the time" 
 
 #Determine if morning
